afd259372d20a7d36d0e3a9b3ac8e8c236de4f6f
1anointedradio-maker <1anointedradio@gmail.com>
2026-05-26 18:48:10 -0400

    Fix spacing in title for Home component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h2>ELHARVEST Web PWA</h2>
+      <h2>EL HARVEST Web PWA</h2>
       <p>Governance + Replay Dashboard Online.</p>
     </div>
   );
